46c5d330ea8af6aaed0e7681ab893f4c5a067f99
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-02-28 17:22:00 +0100

    added the forgot password feature

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -147,17 +147,15 @@ const forgotten = (req, res) => {
 const verifyOTP = (req, res) => {
     const { otp } = req.body;
 
-    LastModel.findOne({ email })
+    LastModel.findOne({ otp })
         .then((user) => {
-            if (user) {
-                if (user.otp && user.otpExpiration && user.otp === otp && user.otpExpiration > new Date()) {
-                    res.status(200).json({ message: 'OTP verified successfully', status: true });
-                } else {
-                    res.status(400).json({ error: 'Invalid or expired OTP' });
-                }
-            } else {
-                res.status(404).json({ error: 'User not found' });
+            if(user.otp == otp && user.otpExpiration > new Date()){
+                
+                res.status(200).json({ message: 'OTP verified successfully', status: true });
+            }else{
+                res.status(400).json({ message: 'invalid OTP', status: false });
             }
+            
         })
         .catch((error) => {
             res.status(500).json({ error: 'Database error' });
